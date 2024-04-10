afd6081086483f774fd7ae9ccfaead684e810825
jhonedin <jhonedin1888@hotmail.com>
2024-04-10 17:44:46 -0500

    Empezamos de cero

--- Usuarios/Controladores/Salir.php
+++
@@ -1,8 +0,0 @@
-<?php
- require_once('../Modelo/Usuarios.php');
-
- $ModeloUsuarios = new Usuarios();
-
- //Llamo el metodo de cerrar la sesion y salir
- $ModeloUsuarios->salir();
-?>
--- conexion.php
+++ conexion.php
@@ -1,49 +1,22 @@
 <?php
+// Datos de conexión a la base de datos
+$servername = "192.168.14.80"; // Cambia "localhost" por la dirección del servidor de tu base de datos
+$username = "jhon"; // Cambia "usuario" por tu nombre de usuario de MySQL
+$password = "UnivalleTulua2023**"; // Cambia "contraseña" por tu contraseña de MySQL
+$database = "bdcontrolasistenciasalas"; // Cambia "nombre_base_de_datos" por el nombre de tu base de datos
 
-class Conexion{
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
 
-  private $server = "mysql:host=192.168.14.80;dbname=bdcontrolasistenciasalas";
-	private $username = "jhon";
-	private $password = "UnivalleTulua2023**";
-	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
-	protected $conn;
-
-	public function open(){
- 		try{
- 			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
- 			return $this->conn;
- 		}
- 		catch (PDOException $e){
- 			echo "Ocurrió un problema con la conexión: " . $e->getMessage();
- 		}
-
-    }
-
-	public function close(){
-   		$this->conn = null;
- 	}
-
-
-    /*
-    protected $db; // atributo que retornar el constructos cada que vez que se haga el llamado desde una clase hija, contendra todo el proceso de conexion
-    private $driver = "mysql";
-    private $host = "localhost";
-    private $bd = "notas";
-    private $usuario = "root";
-    private $contrasena = "";
-
-    public function __construct()
-    {
-        try {
-            $db = PDO("{$this->driver}:host={$this->host};dbname={$this->bd}",$this->usuario,$this->contrasena);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $db;
-
-        } catch (PDOException $e) {
-            echo "Ha surgido un error al tratar de conectarse a la base de datos ".$e->getMenssage();
-        }
-
-    }*/
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+} else {
+    echo "Conexión exitosa";
+    // Aquí puedes realizar consultas, inserciones, actualizaciones, etc.
 }
+
+// Cerrar conexión
+$conn->close();
 
 ?>
