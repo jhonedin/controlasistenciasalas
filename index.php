51214078d53098ba397cb527aee2837f5a4dcff4
jhonedin <jhonedin1888@hotmail.com>
2024-04-26 17:57:57 -0500

    se crea el index

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+
+    <title>CONTROL DE ASISTENCIA SALAS DE SISTEMAS</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body> 
+    <header>
+        <h1>CONTROL DE ASISTENCIA SALAS DE SISTEMAS</h1>
+    </header>
+    <main>
+        
+        
+    </main>
+</body>
+</html>
