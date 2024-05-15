62beb8a7c747ccd0a52ce4172f2dcabd66a59983
jhonedin <jhonedin1888@hotmail.com>
2024-05-15 17:57:41 -0500

    trabajando en las rutas

---
+++ pruebas/PruebasEstudianteController.php
@@ -0,0 +1,7 @@
+<?php    
+
+include('C:\\xampp\\htdocs...');
+include("../ModuloEstudiantes/Controlador/EstudianteController.php");
+include("../ModuloEstudiantes/Modelo/EstudianteModel.php");
+
+?>
