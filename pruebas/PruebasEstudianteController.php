1d86c01e57b50394bacd2236c097a8e5f1733a84
jhonedin <jhonedin1888@hotmail.com>
2024-05-31 13:31:50 -0500

    31 de mayo 2024

---
+++ pruebas/PruebasEstudianteController.php
@@ -0,0 +1,12 @@
+<?php  
+
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].'/');
+
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/controlasistenciasalas/ModuloEstudiantes/Controlador/EstudianteController.php';
+
+// Resto de tu código
+
+
+
+?>
